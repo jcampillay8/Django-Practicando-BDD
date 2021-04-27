532ce492e031b82472f64739c479f917530fb7aa
Jaime Campillay <jcampillayworks@gmail.com>
2021-04-27 12:40:56 -0400

    Django practicando BDD 01

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\jaime\desktop\coding dojo\back-end\python_stack\django\pildoras\video bdd\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
